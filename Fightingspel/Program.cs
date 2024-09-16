88a84682322d5f0ea1856cc4e69574e773b9c4da
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-09-16 09:37:39 +0200

    first commit

---
+++ Fightingspel/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "dean")
+{
+    Console.WriteLine("KYS");
+}
+
+else
+{
+    Console.WriteLine("Välkommen Fighter");
+}
+
+Console.ReadLine();
